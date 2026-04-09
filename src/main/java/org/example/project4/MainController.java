b08d75f3b28fcad1ab5ed27c379d0a7363f704fe
Joe <jguanine@gmail.com>
2026-04-09 16:49:19 -0400

    aksjdksa

--- src/main/java/org/example/project4/MainController.java
+++ src/main/java/org/example/project4/MainController.java
@@ -1,17 +1,41 @@
 package org.example.project4;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+
+import java.io.IOException;
 
 public class MainController {
     @FXML
-    protected void onChicagoClick() {
-        System.out.println("Chicago Style clicked");
-        // later: switch scene
+    private void switchScene(ActionEvent event, String fxmlFile) {
+        try {
+            Parent root = FXMLLoader.load(
+                    getClass().getResource("/org/example/project4/" + fxmlFile)
+            );
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
-    protected void onNYClick() {
-        System.out.println("NY Style clicked");
+    protected void onChicagoClick(ActionEvent event) {
+        switchScene(event, "chicago-view.fxml");
+    }
+
+    @FXML
+    protected void onNYClick(ActionEvent event) {
+        switchScene(event, "ny-view.fxml");
     }
 }
